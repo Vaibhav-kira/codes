a5d402ba27aa7ddda7b84ef45c8b2f0580039d12
Vaibhav ahuja <65072601+Vaibhav-kira@users.noreply.github.com>
2020-12-11 08:59:46 +0530

    project
    Signed-off-by: Vaibhav ahuja <65072601+Vaibhav-kira@users.noreply.github.com>

--- R programming/P2/heart-failure.R
+++ R programming/P2/heart-failure.R
@@ -1,9 +1,9 @@
 library("data.table")
 out <- data.table::fread("outcome-of-care-measures.csv")
-out[, (23) := lapply(.SD, as.numeric), .SDcols = (23)]
+out[, (17) := lapply(.SD, as.numeric), .SDcols = (17)]
 out[, lapply(.SD
              , hist
              , xlab= "Deaths"
-             , main = "Hospital 30-Day Death (Mortality) Rates from heart failure"
+             , main = "Hospital 30-Day Death (Mortality) Rates from Pneumonia"
              , col="red")
-    , .SDcols = (23)]
+    , .SDcols = (17)]
